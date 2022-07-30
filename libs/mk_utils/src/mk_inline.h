0a23f47503063ef2e9527ca2800d5b1e3a606a4e
Marek Knápek <knapek.mar@gmail.com>
2022-07-31 00:58:42 +0200

    Add mk_utils files.

---
+++ libs/mk_utils/src/mk_inline.h
@@ -0,0 +1,15 @@
+#ifndef mk_include_guard_inline
+#define mk_include_guard_inline
+
+
+#include "mk_setup.h"
+
+
+#if mk_uint_has_inline
+#define mk_inline inline
+#else
+#define mk_inline
+#endif
+
+
+#endif
