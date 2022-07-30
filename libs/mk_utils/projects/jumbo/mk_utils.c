2cc19a3a26e0cafc813a1d64786f38029abd1059
Marek Knápek <knapek.mar@gmail.com>
2022-07-31 01:11:12 +0200

    Add mk_utils jumbo.

---
+++ libs/mk_utils/projects/jumbo/mk_utils.c
@@ -0,0 +1,12 @@
+#ifndef mk_include_guard_utils_c
+#define mk_include_guard_utils_c
+
+
+#include "mk_utils.h"
+
+
+#include "../../src/mk_clobber.c"
+#include "../../src/mk_try.c"
+
+
+#endif
---
+++ libs/mk_utils/projects/jumbo/mk_utils.h
@@ -0,0 +1,13 @@
+#ifndef mk_include_guard_utils_h
+#define mk_include_guard_utils_h
+
+
+#include "../../src/mk_assert.h"
+#include "../../src/mk_clobber.h"
+#include "../../src/mk_inline.h"
+#include "../../src/mk_jumbo.h"
+#include "../../src/mk_setup.h"
+#include "../../src/mk_try.h"
+
+
+#endif
