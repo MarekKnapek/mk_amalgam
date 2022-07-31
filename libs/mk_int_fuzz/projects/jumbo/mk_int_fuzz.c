06088f00f986707d4c469f01a25b8363622a75d3
Marek Knápek <knapek.mar@gmail.com>
2022-07-31 03:16:47 +0200

    mk_int_fuzz jumbo

---
+++ libs/mk_int_fuzz/projects/jumbo/mk_int_fuzz.c
@@ -0,0 +1,15 @@
+#ifndef mk_include_guard_int_fuzz_c
+#define mk_include_guard_int_fuzz_c
+
+
+#include "../../src/mk_uint_fuzz.c"
+#include "../../src/mk_uint_fuzz_128.c"
+#include "../../src/mk_uint_fuzz_16.c"
+#include "../../src/mk_uint_fuzz_32.c"
+#include "../../src/mk_uint_fuzz_64.c"
+#include "../../src/mk_uint_fuzz_8.c"
+
+#include "../../../mk_int/projects/jumbo/mk_int.c"
+
+
+#endif
