419b33fa1a0d79ccd67e49a94b847712ff4bc070
Marek Knápek <knapek.mar@gmail.com>
2022-08-06 16:40:19 +0200

    mk_win_base

---
+++ libs/__mk_win/projects/jumbo/mk_win.h
@@ -0,0 +1,3 @@
+#ifndef mk_include_guard_win_h
+#define mk_include_guard_win_h
+#endif
---
+++ libs/mk_win_base/src/mk_win_base_kernel_types.h
@@ -0,0 +1,13 @@
+#ifndef mk_win_base_kernel_types
+#define mk_win_base_kernel_types
+
+
+#include "mk_win_base_keywords.h"
+
+
+struct mk_win_base_kernel_types_hmodule_s; typedef struct mk_win_base_kernel_types_hmodule_s const mk_win_base_keywords_near* mk_win_base_kernel_types_hmodule_t;
+
+typedef int(mk_win_base_keywords_calling_convention_api*mk_win_base_kernel_types_farproc_t)(int);
+
+
+#endif
