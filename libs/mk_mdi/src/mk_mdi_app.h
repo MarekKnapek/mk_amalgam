33fbb20a5c95fb6ea92ed9b388ada2e890bd6a48
Marek Knápek <knapek.mar@gmail.com>
2022-08-15 06:28:06 +0200

    Forward declarations.

--- libs/mk_mdi/src/mk_mdi_app.h
+++ libs/mk_mdi/src/mk_mdi_app.h
@@ -2,22 +2,11 @@
 #define mk_include_guard_mdi_app
 
 
+#include "mk_mdi_forward.h"
+
 #include "../../mk_std/src/mk_std_ptr_buff.h"
 
 #include "../../mk_utils/src/mk_jumbo.h"
-
-
-struct mk_mdi_app_s;
-typedef struct mk_mdi_app_s mk_mdi_app_t;
-typedef mk_mdi_app_t* mk_mdi_app_pt;
-
-struct mk_mdi_parent_s;
-typedef struct mk_mdi_parent_s mk_mdi_parent_t;
-typedef mk_mdi_parent_t* mk_mdi_parent_pt;
-
-struct mk_mdi_child_s;
-typedef struct mk_mdi_child_s mk_mdi_child_t;
-typedef mk_mdi_child_t* mk_mdi_child_pt;
 
 
 struct mk_mdi_app_s
