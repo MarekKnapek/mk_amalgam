a8e4fce68da7be2d62691601b47b8b41e2ddf603
Marek Knápek <knapek.mar@gmail.com>
2022-08-18 19:59:59 +0200

    Refactor.

--- libs/mk_int_fuzz/projects/jumbo/mk_int_fuzz.c
+++ libs/mk_int_fuzz/projects/jumbo/mk_int_fuzz.c
@@ -2,14 +2,25 @@
 #define mk_include_guard_int_fuzz_c
 
 
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable:4464) /* warning C4464: relative include path contains '..' */
+#endif
+
+
 #include "../../src/mk_uint_fuzz.c"
-#include "../../src/mk_uint_fuzz_128.c"
+#include "../../src/mk_uint_fuzz_8.c"
 #include "../../src/mk_uint_fuzz_16.c"
 #include "../../src/mk_uint_fuzz_32.c"
 #include "../../src/mk_uint_fuzz_64.c"
-#include "../../src/mk_uint_fuzz_8.c"
+#include "../../src/mk_uint_fuzz_128.c"
 
 #include "../../../mk_int/projects/jumbo/mk_int.c"
+
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
 
 
 #endif
