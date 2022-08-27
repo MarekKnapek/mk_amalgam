c265795e4a94a50621b35b8540e165ea9ae8129c
Marek Knápek <knapek.mar@gmail.com>
2022-08-27 07:04:47 +0200

    Fix cursor.

--- libs/mk_win/src/mk_win_user_cursor.c
+++ libs/mk_win/src/mk_win_user_cursor.c
@@ -3,14 +3,13 @@
 #include "mk_win_str.h"
 #include "mk_win_unicode.h"
 
+#include "../../mk_utils/src/mk_assert.h"
 #include "../../mk_utils/src/mk_jumbo.h"
 #include "../../mk_utils/src/mk_try.h"
 
 #include "../../mk_win_base/src/mk_win_base_user_functions.h"
 #include "../../mk_win_base/src/mk_win_base_user_types.h"
 #include "../../mk_win_base/src/mk_win_base_keywords.h"
-
-#include <stddef.h>
 
 
 mk_jumbo int mk_win_user_cursor_load_by_name(mk_win_base_types_hinstance_t instance, mk_win_strc_t name, mk_win_base_user_types_hcursor_t* cursor)
@@ -53,30 +52,26 @@ mk_jumbo int mk_win_user_cursor_load_by_name(mk_win_base_types_hinstance_t insta
 
 mk_jumbo int mk_win_user_cursor_load_by_id(unsigned short id, mk_win_base_user_types_hcursor_t* cursor)
 {
-	#if mk_win_api == mk_win_api_old
-	#define LoadCursorA LoadCursor
-	#endif
-
 	mk_win_base_user_types_hcursor_t i;
-	#if mk_win_api == mk_win_api_old || mk_win_api == mk_win_api_ansi
-	i = LoadCursorA(NULL, mk_win_base_user_functions_make_int_resource_a(id));
+	#if mk_win_api == mk_win_api_old
+	i = LoadCursor(mk_win_base_types_null, mk_win_base_user_functions_make_int_resource_a(id));
+	#elif mk_win_api == mk_win_api_ansi
+	i = LoadCursorA(mk_win_base_types_null, mk_win_base_user_functions_make_int_resource_a(id));
 	#elif mk_win_api == mk_win_api_wide
-	i = LoadCursorW(NULL, mk_win_base_user_functions_make_int_resource_w(id));
-	#else
+	i = LoadCursorW(mk_win_base_types_null, mk_win_base_user_functions_make_int_resource_w(id));
+	#elif mk_win_api == mk_win_api_both
 	int unicode_enabled;
 	mk_try(mk_win_unicode_enabled(&unicode_enabled));
 	if(!unicode_enabled)
 	{
-		i = LoadCursorA(NULL, mk_win_base_user_functions_make_int_resource_a(id));
+		i = LoadCursorA(mk_win_base_types_null, mk_win_base_user_functions_make_int_resource_a(id));
 	}
 	else
 	{
-		i = LoadCursorW(NULL, mk_win_base_user_functions_make_int_resource_w(id));
+		i = LoadCursorW(mk_win_base_types_null, mk_win_base_user_functions_make_int_resource_w(id));
 	}
 	#endif
 	mk_assert(cursor);
 	*cursor = i;
 	return 0;
-
-	#undef LoadCursorM
 }
