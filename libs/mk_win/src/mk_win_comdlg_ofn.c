7c6ca69dde9def294d47026d986c0143a341b19b
Marek Knápek <knapek.mar@gmail.com>
2022-08-31 01:37:22 +0200

    Implement open file name.

---
+++ libs/mk_win/src/mk_win_comdlg_ofn.c
@@ -0,0 +1,136 @@
+#include "mk_win_comdlg_ofn.h"
+
+#include "mk_win_api.h"
+#include "mk_win_char.h"
+#include "mk_win_str.h"
+#include "mk_win_unicode.h"
+
+#include "../../mk_win_base/src/mk_win_base_types.h"
+#include "../../mk_win_base/src/mk_win_base_user_types.h"
+#include "../../mk_win_base/src/mk_win_base_comdlg_types.h"
+#include "../../mk_win_base/src/mk_win_base_comdlg_functions.h"
+
+#include "../../mk_utils/src/mk_assert.h"
+#include "../../mk_utils/src/mk_jumbo.h"
+#include "../../mk_utils/src/mk_try.h"
+
+
+mk_jumbo int mk_win_comdlg_ofn(mk_win_comdlg_ofn_t* ofn, mk_win_base_types_bool_t* ret)
+{
+	#if mk_win_api == mk_win_api_old
+	#define GetOpenFileNameM GetOpenFileName
+	#define mk_win_base_comdlg_types_open_file_name_t mk_win_base_comdlg_types_open_file_name_v1_a_t
+	#elif mk_win_api == mk_win_api_ansi
+	#define GetOpenFileNameM GetOpenFileNameA
+	#define mk_win_base_comdlg_types_open_file_name_t mk_win_base_comdlg_types_open_file_name_v1_a_t
+	#elif mk_win_api == mk_win_api_wide
+	#define GetOpenFileNameM GetOpenFileNameW
+	#define mk_win_base_comdlg_types_open_file_name_t mk_win_base_comdlg_types_open_file_name_v1_w_t
+	#endif
+	#if mk_win_api != mk_win_api_both
+	mk_win_base_comdlg_types_open_file_name_t ofnr;
+	mk_win_base_types_bool_t b;
+	mk_assert(ofn);
+	mk_assert(ret);
+	ofnr.m_size = sizeof(ofnr);
+	ofnr.m_owner = ofn->m_owner;
+	ofnr.m_instance = ofn->m_instance;
+	mk_try(mk_win_str_to_api_zz(ofn->m_filter, 0, &ofnr.m_filter));
+	mk_try(mk_win_str_to_api_zz(ofn->m_custom_filter, 1, &ofnr.m_custom_filter));
+	ofnr.m_max_custom_tfilter = ofn->m_max_custom_tfilter;
+	ofnr.m_filter_index = ofn->m_filter_index;
+	mk_try(mk_win_str_to_api_s(ofn->m_file_name_buff, ofn->m_file_name_buff_len_chars, 2, &ofnr.m_file));
+	ofnr.m_max_file = ofn->m_file_name_buff_len_chars;
+	mk_try(mk_win_str_to_api_z(ofn->m_file_title, 3, &ofnr.m_file_title));
+	ofnr.m_max_file_title = ofn->m_max_file_title;
+	mk_try(mk_win_str_to_api_z(ofn->m_initial_dir, 4, &ofnr.m_initial_dir));
+	mk_try(mk_win_str_to_api_z(ofn->m_title, 5, &ofnr.m_title));
+	ofnr.m_flags = ofn->m_flags;
+	ofnr.m_file_offset = ofn->m_file_offset;
+	ofnr.m_file_extension = ofn->m_file_extension;
+	mk_try(mk_win_str_to_api_z(ofn->m_default_extension, 6, &ofnr.m_default_extension));
+	ofnr.m_custom_data = ofn->m_custom_data;
+	ofnr.m_hook = ofn->m_hook;
+	mk_try(mk_win_str_to_api_z(ofn->m_template_name, 7, &ofnr.m_template_name));
+	b = GetOpenFileNameM(&ofnr);
+	mk_try(mk_win_str_from_api_z(ofnr.m_custom_filter, 1, &ofn->m_custom_filter));
+	mk_try(mk_win_str_from_api_s(ofnr.m_file, ofn->m_file_name_buff_len_chars, 2, &ofn->m_file_name_out));
+	mk_try(mk_win_str_from_api_z(ofnr.m_file_title, 3, &ofn->m_file_title));
+	mk_assert(ret);
+	*ret = b;
+	return 0;
+	#else
+	int unicode_enabled;
+	mk_try(mk_win_unicode_enabled(&unicode_enabled));
+	if(!unicode_enabled)
+	{
+		mk_win_base_comdlg_types_open_file_name_v1_a_t ofnr;
+		mk_win_base_types_bool_t b;
+		mk_assert(ofn);
+		mk_assert(ret);
+		ofnr.m_size = sizeof(ofnr);
+		ofnr.m_owner = ofn->m_owner;
+		ofnr.m_instance = ofn->m_instance;
+		mk_try(mk_win_str_to_narrow_zz(ofn->m_filter, 0, &ofnr.m_filter));
+		mk_try(mk_win_str_to_narrow_zz(ofn->m_custom_filter, 1, &ofnr.m_custom_filter));
+		ofnr.m_max_custom_tfilter = ofn->m_max_custom_tfilter;
+		ofnr.m_filter_index = ofn->m_filter_index;
+		mk_try(mk_win_str_to_narrow_s(ofn->m_file_name_buff, ofn->m_file_name_buff_len_chars, 2, &ofnr.m_file));
+		ofnr.m_max_file = ofn->m_file_name_buff_len_chars;
+		mk_try(mk_win_str_to_narrow_z(ofn->m_file_title, 3, &ofnr.m_file_title));
+		ofnr.m_max_file_title = ofn->m_max_file_title;
+		mk_try(mk_win_str_to_narrow_z(ofn->m_initial_dir, 4, &ofnr.m_initial_dir));
+		mk_try(mk_win_str_to_narrow_z(ofn->m_title, 5, &ofnr.m_title));
+		ofnr.m_flags = ofn->m_flags;
+		ofnr.m_file_offset = ofn->m_file_offset;
+		ofnr.m_file_extension = ofn->m_file_extension;
+		mk_try(mk_win_str_to_narrow_z(ofn->m_default_extension, 6, &ofnr.m_default_extension));
+		ofnr.m_custom_data = ofn->m_custom_data;
+		ofnr.m_hook = ofn->m_hook;
+		mk_try(mk_win_str_to_narrow_z(ofn->m_template_name, 7, &ofnr.m_template_name));
+		b = GetOpenFileNameA(&ofnr);
+		mk_try(mk_win_str_from_narrow_z(ofnr.m_custom_filter, 1, &ofn->m_custom_filter));
+		mk_try(mk_win_str_from_narrow_s(ofnr.m_file, ofn->m_file_name_buff_len_chars, 2, &ofn->m_file_name_out));
+		mk_try(mk_win_str_from_narrow_z(ofnr.m_file_title, 3, &ofn->m_file_title));
+		mk_assert(ret);
+		*ret = b;
+		return 0;
+	}
+	else
+	{
+		mk_win_base_comdlg_types_open_file_name_v1_w_t ofnr;
+		mk_win_base_types_bool_t b;
+		mk_assert(ofn);
+		mk_assert(ret);
+		ofnr.m_size = sizeof(ofnr);
+		ofnr.m_owner = ofn->m_owner;
+		ofnr.m_instance = ofn->m_instance;
+		mk_try(mk_win_str_to_wide_zz(ofn->m_filter, 0, &ofnr.m_filter));
+		mk_try(mk_win_str_to_wide_zz(ofn->m_custom_filter, 1, &ofnr.m_custom_filter));
+		ofnr.m_max_custom_tfilter = ofn->m_max_custom_tfilter;
+		ofnr.m_filter_index = ofn->m_filter_index;
+		mk_try(mk_win_str_to_wide_s(ofn->m_file_name_buff, ofn->m_file_name_buff_len_chars, 2, &ofnr.m_file));
+		ofnr.m_max_file = ofn->m_file_name_buff_len_chars;
+		mk_try(mk_win_str_to_wide_z(ofn->m_file_title, 3, &ofnr.m_file_title));
+		ofnr.m_max_file_title = ofn->m_max_file_title;
+		mk_try(mk_win_str_to_wide_z(ofn->m_initial_dir, 4, &ofnr.m_initial_dir));
+		mk_try(mk_win_str_to_wide_z(ofn->m_title, 5, &ofnr.m_title));
+		ofnr.m_flags = ofn->m_flags;
+		ofnr.m_file_offset = ofn->m_file_offset;
+		ofnr.m_file_extension = ofn->m_file_extension;
+		mk_try(mk_win_str_to_wide_z(ofn->m_default_extension, 6, &ofnr.m_default_extension));
+		ofnr.m_custom_data = ofn->m_custom_data;
+		ofnr.m_hook = ofn->m_hook;
+		mk_try(mk_win_str_to_wide_z(ofn->m_template_name, 7, &ofnr.m_template_name));
+		b = GetOpenFileNameW(&ofnr);
+		mk_try(mk_win_str_from_wide_z(ofnr.m_custom_filter, 1, &ofn->m_custom_filter));
+		mk_try(mk_win_str_from_wide_s(ofnr.m_file, ofn->m_file_name_buff_len_chars, 2, &ofn->m_file_name_out));
+		mk_try(mk_win_str_from_wide_z(ofnr.m_file_title, 3, &ofn->m_file_title));
+		mk_assert(ret);
+		*ret = b;
+		return 0;
+	}
+	#endif
+	#undef GetOpenFileNameM
+	#undef mk_win_base_comdlg_types_open_file_name_t
+}
---
+++ libs/mk_win/src/mk_win_comdlg_ofn.h
@@ -0,0 +1,71 @@
+#ifndef mk_include_guard_win_comdlg_ofn
+#define mk_include_guard_win_comdlg_ofn
+
+
+#include "mk_win_char.h"
+
+#include "../../mk_win_base/src/mk_win_base_types.h"
+#include "../../mk_win_base/src/mk_win_base_user_types.h"
+#include "../../mk_win_base/src/mk_win_base_comdlg_types.h"
+
+#include "../../mk_utils/src/mk_jumbo.h"
+
+
+#define mk_win_comdlg_ofn_flag_readonly             0x00000001ul
+#define mk_win_comdlg_ofn_flag_overwriteprompt      0x00000002ul
+#define mk_win_comdlg_ofn_flag_hidereadonly         0x00000004ul
+#define mk_win_comdlg_ofn_flag_nochangedir          0x00000008ul
+#define mk_win_comdlg_ofn_flag_showhelp             0x00000010ul
+#define mk_win_comdlg_ofn_flag_enablehook           0x00000020ul
+#define mk_win_comdlg_ofn_flag_enabletemplate       0x00000040ul
+#define mk_win_comdlg_ofn_flag_enabletemplatehandle 0x00000080ul
+#define mk_win_comdlg_ofn_flag_novalidate           0x00000100ul
+#define mk_win_comdlg_ofn_flag_allowmultiselect     0x00000200ul
+#define mk_win_comdlg_ofn_flag_extensiondifferent   0x00000400ul
+#define mk_win_comdlg_ofn_flag_pathmustexist        0x00000800ul
+#define mk_win_comdlg_ofn_flag_filemustexist        0x00001000ul
+#define mk_win_comdlg_ofn_flag_createprompt         0x00002000ul
+#define mk_win_comdlg_ofn_flag_shareaware           0x00004000ul
+#define mk_win_comdlg_ofn_flag_noreadonlyreturn     0x00008000ul
+#define mk_win_comdlg_ofn_flag_notestfilecreate     0x00010000ul
+#define mk_win_comdlg_ofn_flag_nonetworkbutton      0x00020000ul
+#define mk_win_comdlg_ofn_flag_nolongnames          0x00040000ul
+#define mk_win_comdlg_ofn_flag_explorer             0x00080000ul /* winver >= 0x0400 */
+#define mk_win_comdlg_ofn_flag_nodereferencelinks   0x00100000ul /* winver >= 0x0400 */
+#define mk_win_comdlg_ofn_flag_longnames            0x00200000ul /* winver >= 0x0400 */
+#define mk_win_comdlg_ofn_flag_enableincludenotify  0x00400000ul /* winver >= 0x0400 */
+#define mk_win_comdlg_ofn_flag_enablesizing         0x00800000ul /* winver >= 0x0400 */
+#define mk_win_comdlg_ofn_flag_dontaddtorecent      0x02000000ul /* winnt >= 0x0500 */
+#define mk_win_comdlg_ofn_flag_forceshowhidden      0x10000000ul /* winnt >= 0x0500 */
+
+
+struct mk_win_comdlg_ofn_s
+{
+	mk_win_base_user_types_hwnd_t m_owner;
+  mk_win_base_types_hinstance_t m_instance;
+	mk_win_strc_t m_filter;
+  mk_win_str_t m_custom_filter;
+  mk_win_base_types_dword_t m_max_custom_tfilter;
+  mk_win_base_types_dword_t m_filter_index;
+  mk_win_strc_t m_file_name_buff;
+  mk_win_base_types_dword_t m_file_name_buff_len_chars;
+	mk_win_strc_t m_file_name_out;
+  mk_win_str_t m_file_title;
+  mk_win_base_types_dword_t m_max_file_title;
+  mk_win_strc_t m_initial_dir;
+  mk_win_strc_t m_title;
+  mk_win_base_types_dword_t m_flags;
+  mk_win_base_types_word_t m_file_offset;
+  mk_win_base_types_word_t m_file_extension;
+  mk_win_strc_t m_default_extension;
+  mk_win_base_user_types_lparam_t m_custom_data;
+  mk_win_base_comdlg_types_open_file_name_hook_t m_hook;
+  mk_win_strc_t m_template_name;
+};
+mk_win_base_types_make_struct(mk_win_comdlg_ofn);
+
+
+mk_jumbo int mk_win_comdlg_ofn(mk_win_comdlg_ofn_t* ofn, mk_win_base_types_bool_t* ret);
+
+
+#endif
