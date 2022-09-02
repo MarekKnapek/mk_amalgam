e0c6ffee6529aa5070aa45cf00d8ce107d79861d
Marek Knápek <knapek.mar@gmail.com>
2022-09-02 02:50:30 +0200

    istr & istrg

--- libs/mk_std/projects/jumbo/mk_std.c
+++ libs/mk_std/projects/jumbo/mk_std.c
@@ -6,9 +6,14 @@
 #include "../../src/mk_std_gcallocator.c"
 #include "../../src/mk_std_global.c"
 #include "../../src/mk_std_input_stream.c"
+#include "../../src/mk_std_istr.c"
+#include "../../src/mk_std_istr_storage.c"
+#include "../../src/mk_std_istrg.c"
 #include "../../src/mk_std_ptr_buff.c"
 #include "../../src/mk_std_str_convertor.c"
 
 #include "../../../mk_utils/projects/jumbo/mk_utils.c"
+#include "../../../mk_int/projects/jumbo/mk_int.c"
+
 
 #endif
