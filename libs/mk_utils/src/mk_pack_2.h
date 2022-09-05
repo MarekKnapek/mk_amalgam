2dc58349c6fd59cd1501106d90d34a7f1f285a3e
Marek Knápek <knapek.mar@gmail.com>
2022-09-05 23:53:30 +0200

    Pack.

---
+++ libs/mk_utils/src/mk_pack_2.h
@@ -0,0 +1,15 @@
+#ifndef mk_include_guard_pack_2
+#define mk_include_guard_pack_2
+
+
+#if defined(_MSC_VER)
+#if _MSC_VER >= 900
+#pragma warning(disable:4103) /* warning c4103: alignment changed after including header, may be due to missing #pragma pack(pop) */
+#pragma pack(push,2)
+#else
+#pragma pack(2)
+#endif
+#endif
+
+
+#endif
