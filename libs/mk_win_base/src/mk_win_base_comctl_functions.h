0bf037e8bfc7e7a409c315c3cb999b1394740553
Marek Knápek <knapek.mar@gmail.com>
2022-09-14 08:46:47 +0200

    InitCommonControls

---
+++ libs/mk_win_base/src/mk_win_base_comctl_functions.h
@@ -0,0 +1,13 @@
+#ifndef mk_include_guard_win_base_comctl_functions
+#define mk_include_guard_win_base_comctl_functions
+
+
+#include "mk_win_base_keywords.h"
+
+#include "../../mk_utils/src/mk_cpp.h"
+
+
+mk_extern_c mk_win_base_keywords_dll_import void mk_win_base_keywords_calling_convention_api InitCommonControls(void);
+
+
+#endif
