bd08e656a09578b784259dafd16d9e5e4621c09c
Marek Knápek <knapek.mar@gmail.com>
2022-09-30 05:46:10 +0200

    First read then allocate.
    Rather fail reading than allocating.

--- libs/mk_dacdbt/src/mk_dacdbt_io.h
+++ libs/mk_dacdbt/src/mk_dacdbt_io.h
@@ -2,6 +2,7 @@
 #define mk_include_guard_dacdbt_io
 
 
+#include "../../mk_std/src/mk_std_buffer.h"
 #include "../../mk_std/src/mk_std_input_stream.h"
 
 #include "../../mk_int/src/exact/mk_uint_8.h"
@@ -15,6 +16,7 @@
 
 
 mk_jumbo int mk_dacdbt_io_read_buff(mk_std_input_stream_t* is, void* buff, size_t len);
+mk_jumbo int mk_dacdbt_io_read_buffer(mk_std_input_stream_t* is, mk_std_buffer_t* buffer, size_t len);
 mk_jumbo int mk_dacdbt_io_read_u8(mk_std_input_stream_t* is, mk_uint8_t* u8);
 mk_jumbo int mk_dacdbt_io_read_u16(mk_std_input_stream_t* is, mk_uint16_t* u16);
 mk_jumbo int mk_dacdbt_io_read_u32(mk_std_input_stream_t* is, mk_uint32_t* u32);
