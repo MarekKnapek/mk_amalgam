7e87513babfe2b691f7f6161bfa0b3c1c983ed3e
Marek Knápek <knapek.mar@gmail.com>
2022-10-01 09:59:57 +0200

    Start using status bar.

--- libs/mk_dacdbtw/src/mk_dacdbtw_panel.h
+++ libs/mk_dacdbtw/src/mk_dacdbtw_panel.h
@@ -2,29 +2,7 @@
 #define mk_include_guard_dacdbtw_panel
 
 
-#include "../../mk_dacdbt/src/mk_dacdbt_doc.h"
-
-#include "../../mk_win/src/mk_win_char.h"
-#include "../../mk_win/src/mk_win_user_window.h"
-
-#include "../../mk_win_base/src/mk_win_base_types.h"
-#include "../../mk_win_base/src/mk_win_base_user_types.h"
-
 #include "../../mk_utils/src/mk_jumbo.h"
-
-
-struct mk_dacdbtw_panel_s
-{
-	mk_win_base_user_types_hwnd_t m_hwnd;
-	mk_win_base_user_types_hwnd_t m_tree;
-	mk_win_base_user_types_hwnd_t m_label;
-	int m_state;
-	mk_win_char_t* m_file_name;
-	mk_dacdbt_doc_t m_doc;
-	mk_std_buffer_t m_tree_callback_texts[4];
-	unsigned int m_tree_callback_texts_idx;
-};
-mk_win_base_types_make_struct(mk_dacdbtw_panel);
 
 
 #define mk_dacdbtw_panel_wm_on_idle       ((unsigned short)(mk_win_user_window_wm_user + 0x0001ul))
