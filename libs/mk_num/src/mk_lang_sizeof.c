0b0a0c138fe3039594a2756991c1643790231f19
Marek Knápek <knapek.mar@gmail.com>
2022-11-10 06:05:26 +0100

    Add sizeof basic types.

---
+++ libs/mk_num/src/mk_lang_sizeof.c
@@ -0,0 +1,33 @@
+#include "mk_lang_sizeof.h"
+
+#include "mk_lang_bool.h"
+#include "mk_lang_i128.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_llong.h"
+#include "mk_lang_nodiscard.h"
+
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_bool_t mk_lang_sizeof_test(void)
+{
+	mk_lang_bool_t ret;
+
+	ret = mk_lang_true;
+
+	ret = ret && (mk_lang_sizeof_uchar == 1);
+	ret = ret && (mk_lang_sizeof_uchar <= mk_lang_sizeof_ushort);
+	ret = ret && (mk_lang_sizeof_ushort <= mk_lang_sizeof_uint);
+	ret = ret && (mk_lang_sizeof_uint <= mk_lang_sizeof_ulong);
+	ret = ret && (mk_lang_sizeof_ulong <= mk_lang_sizeof_ullong);
+
+	ret = ret && (sizeof(unsigned char) == mk_lang_sizeof_uchar);
+	ret = ret && (sizeof(unsigned short int) == mk_lang_sizeof_ushort);
+	ret = ret && (sizeof(unsigned int) == mk_lang_sizeof_uint);
+	ret = ret && (sizeof(unsigned long int) == mk_lang_sizeof_ulong);
+	#if mk_lang_llong_has != 0
+	ret = ret && (sizeof(unsigned long long int) == mk_lang_sizeof_ullong);
+	#endif
+	#if mk_lang_i128_has != 0
+	ret = ret && (sizeof(unsigned __int128) == mk_lang_sizeof_ui128t);
+	#endif
+	return ret;
+}
---
+++ libs/mk_num/src/mk_lang_sizeof.h
@@ -0,0 +1,21 @@
+#ifndef mk_detail_include_guard_lang_sizeof
+#define mk_detail_include_guard_lang_sizeof
+
+
+#include "mk_lang_bool.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+
+
+#define mk_lang_sizeof_uchar 1
+#define mk_lang_sizeof_ushort 2
+#define mk_lang_sizeof_uint 4
+#define mk_lang_sizeof_ulong 4
+#define mk_lang_sizeof_ullong 8
+#define mk_lang_sizeof_ui128t 16
+
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_bool_t mk_lang_sizeof_test(void);
+
+
+#endif
