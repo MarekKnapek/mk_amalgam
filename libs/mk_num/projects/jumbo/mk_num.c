08f715928c4e9175765713778be522a26e7fe529
Marek Knápek <knapek.mar@gmail.com>
2022-11-10 12:04:21 +0100

    Add mk_num project.

---
+++ libs/mk_num/projects/jumbo/mk_num.c
@@ -0,0 +1,12 @@
+#ifndef mk_detail_include_guard_num_jumbo_c
+#define mk_detail_include_guard_num_jumbo_c
+
+
+#include "../../src/mk_lang_crash.c"
+#include "../../src/mk_lang_sizeof.c"
+#include "../../src/mk_num_basic_uints.c"
+#include "../../src/mk_num_composite_uints.c"
+#include "../../src/mk_num_div_mod.c"
+
+
+#endif
---
+++ libs/mk_num/projects/jumbo/mk_num.h
@@ -0,0 +1,33 @@
+#ifndef mk_detail_include_guard_num_jumbo_h
+#define mk_detail_include_guard_num_jumbo_h
+
+
+#include "../../src/mk_lang_assert.h"
+#include "../../src/mk_lang_bool.h"
+#include "../../src/mk_lang_charbit.h"
+#include "../../src/mk_lang_concat.h"
+#include "../../src/mk_lang_cpp.h"
+#include "../../src/mk_lang_crash.h"
+#include "../../src/mk_lang_i128.h"
+#include "../../src/mk_lang_inline.h"
+#include "../../src/mk_lang_jumbo.h"
+#include "../../src/mk_lang_limits.h"
+#include "../../src/mk_lang_llong.h"
+#include "../../src/mk_lang_minmax.h"
+#include "../../src/mk_lang_nodiscard.h"
+#include "../../src/mk_lang_sizeof.h"
+#include "../../src/mk_lang_sizet.h"
+#include "../../src/mk_lang_uintmaxt.h"
+#include "../../src/mk_num_basic_uint_def.h"
+#include "../../src/mk_num_basic_uint_undef.h"
+#include "../../src/mk_num_basic_uints.h"
+#include "../../src/mk_num_composite_guess_base.h"
+#include "../../src/mk_num_composite_uint_base_def.h"
+#include "../../src/mk_num_composite_uint_base_undef.h"
+#include "../../src/mk_num_composite_uint_def.h"
+#include "../../src/mk_num_composite_uint_undef.h"
+#include "../../src/mk_num_composite_uints.h"
+#include "../../src/mk_num_div_mod.h"
+
+
+#endif
