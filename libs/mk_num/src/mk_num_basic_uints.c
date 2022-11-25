20e4f0a252f5d2c433bc9d1c02be1ee46364eefb
Marek Knápek <knapek.mar@gmail.com>
2022-11-25 04:58:21 +0100

    Always have basic llong, lllong, intmax.

--- libs/mk_num/src/mk_num_basic_uints.c
+++ libs/mk_num/src/mk_num_basic_uints.c
@@ -22,24 +22,18 @@
 #define mk_detail_num_basic_ut_name ulong
 #include "mk_num_basic_uint.inl.c"
 
-#if mk_lang_llong_has != 0
 #define mk_detail_num_basic_ut_type mk_lang_ullong_t
 #define mk_detail_num_basic_ut_name ullong
 #include "mk_num_basic_uint.inl.c"
-#endif
+
+#define mk_detail_num_basic_ut_type mk_lang_ulllong_t
+#define mk_detail_num_basic_ut_name ulllong
+#include "mk_num_basic_uint.inl.c"
 
 #define mk_detail_num_basic_ut_type size_t
 #define mk_detail_num_basic_ut_name usize
 #include "mk_num_basic_uint.inl.c"
 
-#if mk_lang_uintmaxt_has != 0
 #define mk_detail_num_basic_ut_type uintmax_t
 #define mk_detail_num_basic_ut_name umax
 #include "mk_num_basic_uint.inl.c"
-#endif
-
-#if mk_lang_lllong_has != 0
-#define mk_detail_num_basic_ut_type mk_lang_ulllong_t
-#define mk_detail_num_basic_ut_name ulllong
-#include "mk_num_basic_uint.inl.c"
-#endif
--- libs/mk_num/src/mk_num_basic_uints.h
+++ libs/mk_num/src/mk_num_basic_uints.h
@@ -24,27 +24,21 @@
 #define mk_detail_num_basic_ut_name ulong
 #include "mk_num_basic_uint.inl.h"
 
-#if mk_lang_llong_has != 0
 #define mk_detail_num_basic_ut_type mk_lang_ullong_t
 #define mk_detail_num_basic_ut_name ullong
 #include "mk_num_basic_uint.inl.h"
-#endif
+
+#define mk_detail_num_basic_ut_type mk_lang_ulllong_t
+#define mk_detail_num_basic_ut_name ulllong
+#include "mk_num_basic_uint.inl.h"
 
 #define mk_detail_num_basic_ut_type size_t
 #define mk_detail_num_basic_ut_name usize
 #include "mk_num_basic_uint.inl.h"
 
-#if mk_lang_uintmaxt_has != 0
 #define mk_detail_num_basic_ut_type uintmax_t
 #define mk_detail_num_basic_ut_name umax
 #include "mk_num_basic_uint.inl.h"
-#endif
-
-#if mk_lang_lllong_has != 0
-#define mk_detail_num_basic_ut_type mk_lang_ulllong_t
-#define mk_detail_num_basic_ut_name ulllong
-#include "mk_num_basic_uint.inl.h"
-#endif
 
 
 #endif
