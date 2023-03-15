00ccb872c61f5f2b13cf63e6c6a9773eaa1c31ea
Marek Knápek <knapek.mar@gmail.com>
2023-03-15 16:57:47 +0100

    Better union.

--- libs/mk_dacdbt/src/mk_dacdbt_doc.h
+++ libs/mk_dacdbt/src/mk_dacdbt_doc.h
@@ -29,15 +29,18 @@ struct mk_dacdbt_doc_hdr_v2_s
 };
 typedef struct mk_dacdbt_doc_hdr_v2_s mk_dacdbt_doc_hdr_v2_t;
 
+union mk_dacdbt_doc_hdr_vn_e
+{
+	mk_dacdbt_doc_hdr_v1_t m_hdr1;
+	mk_dacdbt_doc_hdr_v2_t m_hdr2;
+};
+typedef union mk_dacdbt_doc_hdr_vn_e mk_dacdbt_doc_hdr_vn_t;
+
 
 struct mk_dacdbt_doc_s
 {
 	int m_format;
-	union
-	{
-		mk_dacdbt_doc_hdr_v1_t m_hdr1;
-		mk_dacdbt_doc_hdr_v2_t m_hdr2;
-	} m_hdr;
+	mk_dacdbt_doc_hdr_vn_t m_hdr;
 	mk_dacdbt_key_t m_root;
 };
 typedef struct mk_dacdbt_doc_s mk_dacdbt_doc_t;
