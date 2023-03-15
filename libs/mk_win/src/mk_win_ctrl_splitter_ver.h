4e42a3570c86ccd9803663e49bf4aa98b4595178
Marek Knápek <knapek.mar@gmail.com>
2023-03-15 15:11:34 +0100

    mk_win_ctrl_splitter_ver

---
+++ libs/mk_win/src/mk_win_ctrl_splitter_ver.h
@@ -0,0 +1,23 @@
+#ifndef mk_include_guard_win_ctrl_splitter_ver
+#define mk_include_guard_win_ctrl_splitter_ver
+
+
+#include "../../mk_utils/src/mk_jumbo.h"
+
+#include "../../mk_win_base/src/mk_win_base_types.h"
+
+#include "../../mk_win/src/mk_win_char.h"
+#include "../../mk_win/src/mk_win_user_window.h"
+
+
+#define mk_win_ctrl_splitter_ver_wm_set_ratio ((unsigned short)(mk_win_user_window_wm_user + 0x0001ul))
+#define mk_win_ctrl_splitter_ver_wm_set_child ((unsigned short)(mk_win_user_window_wm_user + 0x0002ul))
+
+
+mk_jumbo int mk_win_ctrl_splitter_ver_class_register(void);
+mk_jumbo int mk_win_ctrl_splitter_ver_class_unregister(void);
+mk_jumbo int mk_win_ctrl_splitter_ver_class_get_atom(mk_win_base_types_atom_t* const atom);
+mk_jumbo int mk_win_ctrl_splitter_ver_class_get_name(mk_win_strc_t* const name);
+
+
+#endif
